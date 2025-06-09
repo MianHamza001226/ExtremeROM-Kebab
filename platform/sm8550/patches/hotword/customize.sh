12d462f4d0bc1e58940fbeca0d76c86c183cbc6b
osrott61-gh <173093235+osrott61-gh@users.noreply.github.com>
2025-06-10 00:56:46 +0200

    will this work? i have no idea

--- platform/sm8550/patches/hotword/customize.sh
+++ platform/sm8550/patches/hotword/customize.sh
@@ -4,8 +4,3 @@ DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentYGoogleEx6_WIDEBAND_LA
 if [[ "$TARGET_CODENAME" != "r8s" ]]; then
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON/HotwordEnrollmentOKGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx4HEXAGON/HotwordEnrollmentXGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
-
-
-echo Finished!
-
-done
