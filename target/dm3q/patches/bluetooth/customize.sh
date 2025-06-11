5953b34699452ff1945fb3315027677e83d11b66
Your Name <you@example.com>
2025-06-11 20:57:33 +0200

    dm3q: bt patches: remove unneccesary lines
    Signed-off-by: Your Name <you@example.com>

--- target/dm3q/patches/bluetooth/customize.sh
+++ target/dm3q/patches/bluetooth/customize.sh
@@ -9,5 +9,3 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libaudiopolicy.so" 0 0
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libaudiopolicymanagerdefault.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libaudiopolicyenginedefault.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libaudiopolicycomponents.so" 0 0 644 "u:object_r:system_lib_file:s0"
-
-
