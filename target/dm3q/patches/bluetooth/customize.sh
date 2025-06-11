b153ef498b8b21a4c88dfe01cf19819bebf211ef
osrott61-gh <osrott61@dev.extremerom.org>
2025-06-11 15:57:53 +0200

    dm3q: fix bluetooth, again

--- target/dm3q/patches/bluetooth/customize.sh
+++ target/dm3q/patches/bluetooth/customize.sh
@@ -1,3 +1,6 @@
+DELETE_FROM_WORK_DIR "system" "system/apex/com.android.btservices.apex"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libbluetooth_jni.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libbluetooth_jni.so" 0 0 644 "uu:object_r:system_lib_file:s0"
+
 
