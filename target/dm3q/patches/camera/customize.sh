de579f4e03000b0221dca67ce9edae801f923088
osrott61-gh <osrott61@dev.extremerom.org>
2025-06-13 16:20:53 +0200

    fix camera part 3000

--- target/dm3q/patches/camera/customize.sh
+++ target/dm3q/patches/camera/customize.sh
@@ -4,3 +4,4 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.s
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libmacroclearshot_raw.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libmacroclearshot_raw_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
