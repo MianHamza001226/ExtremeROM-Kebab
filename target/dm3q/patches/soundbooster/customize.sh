83dfa0ee1e76ec66a0e2f7a9a1b2426b3e848f27
osrott61-gh <osrott61@dev.extremerom.org>
2025-06-16 23:22:05 +0200

    remove custom soundbooster
    Signed-off-by: osrott61-gh <osrott61@dev.extremerom.org>

--- target/dm3q/patches/soundbooster/customize.sh
+++
@@ -1,2 +0,0 @@
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1100.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
